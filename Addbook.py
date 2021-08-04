91711faf5f774e653ec0b056c398050f782abda8
ritik <ritikcn05@gmail.com>
2021-08-04 22:30:17 +0530

    Correcting the databse password

--- Addbook.py
+++ Addbook.py
@@ -37,7 +37,7 @@ def addBook():
     root.geometry("600x500")
 
     # Add your own database name and password here to reflect in the code
-    mypass = "root"
+    mypass = "password"
     mydatabase="db"
 
     con = pymysql.connect(host="localhost",user="root",password=mypass,database=mydatabase)
